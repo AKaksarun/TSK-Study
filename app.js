7461aa4b6d71a8a24531e51aa098126c78eda5bb
AKaksarun <arunkumarsubramaniyan2.0@gmail.com>
2026-07-07 11:57:06 +0530

    push to hub

---
+++ app.js
@@ -0,0 +1,11 @@
+const version = "v1.0";
+
+document.getElementById("version").innerHTML = version;
+
+function deploy(){
+
+document.getElementById("message").innerHTML =
+
+"✅ Deployment Successful! GitHub Actions triggered a new deployment and the latest Docker container is now running on Amazon EC2.";
+
+}
